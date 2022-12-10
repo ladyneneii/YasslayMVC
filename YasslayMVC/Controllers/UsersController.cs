8e46155190e19041c26b04cfcbfa8bd161de437d
nene <ernestcurativo@gmail.com>
2022-12-10 14:53:23 +0800

    Added Create

---
+++ YasslayMVC/Controllers/UsersController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using YasslayMVC.Models;
+
+namespace YasslayMVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        string connectionString = @"Data Source=DESKTOP-9MQ1M1N\SQLEXPRESS;Initial Catalog=Yasslay;Integrated Security=True";
+        [HttpGet]
+        public ActionResult Index()
+        {
+            DataTable dtblUsers = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM UsersTable", sqlCon);
+                sqlDa.Fill(dtblUsers);
+
+            }
+            return View(dtblUsers);
+        }
+
+        [HttpGet]
+        // GET: UsersController/Create
+        public ActionResult Create()
+        {
+            return View(new UsersModel());
+        }
+
+        // POST: UsersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ YasslayMVC/Models/UsersModel.cs
@@ -0,0 +1,13 @@
+﻿namespace YasslayMVC.Models
+{
+    public class UsersModel
+    {
+        public int UserID { get; set; } 
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public string UserType { get; set; } = default!;
+        public string State { get; set; } = default!;
+    }
+}
