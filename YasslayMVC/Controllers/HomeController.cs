937078512ce62a4b1e41769d15a97aa42c2617cb
nene <ernestcurativo@gmail.com>
2022-12-11 11:02:34 +0800

    Partially added Register

--- YasslayMVC/Controllers/HomeController.cs
+++ YasslayMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using YasslayMVC.Models;
 
@@ -6,6 +7,8 @@ namespace YasslayMVC.Controllers
 {
     public class HomeController : Controller
     {
+        string connectionString = @"Data Source=DESKTOP-9MQ1M1N\SQLEXPRESS;Initial Catalog=Yasslay;Integrated Security=True";
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -23,9 +26,32 @@ namespace YasslayMVC.Controllers
             return View();
         }
 
-        public IActionResult Signup()
+        [HttpGet]
+        // GET: HomeController/Register
+        public ActionResult Register()
         {
-            return View();
+            return View(new UsersModel());
+        }
+
+        // POST: HomeController/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(UsersModel usersModel)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                string query = "INSERT INTO UsersTable VALUES(@FirstName,@LastName,@Email,@Password,@UserType,@State)";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.Parameters.AddWithValue("@FirstName", usersModel.FirstName);
+                sqlCmd.Parameters.AddWithValue("@LastName", usersModel.LastName);
+                sqlCmd.Parameters.AddWithValue("@Email", usersModel.Email);
+                sqlCmd.Parameters.AddWithValue("@Password", usersModel.Password);
+                sqlCmd.Parameters.AddWithValue("@UserType", usersModel.UserType);
+                sqlCmd.Parameters.AddWithValue("@State", usersModel.State);
+                sqlCmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
