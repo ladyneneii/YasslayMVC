55628828fd66d0d2d1f23d42895b759f67802aba
nene <ernestcurativo@gmail.com>
2022-12-11 10:40:50 +0800

    Added All Users to navigation bar

--- YasslayMVC/Controllers/HomeController.cs
+++ YasslayMVC/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace YasslayMVC.Controllers
             return View();
         }
 
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
